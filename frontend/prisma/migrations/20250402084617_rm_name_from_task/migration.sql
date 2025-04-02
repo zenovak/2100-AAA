c30d47a30b6b34ed01f7831822bccb559581127b
Zenovak <xe.novak@outlook.com>
2025-04-02 16:47:31 +0800

    fix: removed redudant task name field from db schema

---
+++ frontend/prisma/migrations/20250402084617_rm_name_from_task/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Task" DROP COLUMN "name";
